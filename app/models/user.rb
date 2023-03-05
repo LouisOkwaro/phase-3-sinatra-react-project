b9ca79547d10456a0241ad9f2c5827dcfc2047dd
LouisOkwaro <okwarolouis@gmail.comgit config --global user.email okwarolouis@gmail.com>
2023-03-05 16:06:19 +0300

    updated user.rb file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,1 +1,13 @@
+require 'bcrypt'
 
+class User < ActiveRecord::Base
+  has_many :projects
+  has_many :skills
+
+  has_secure_password
+  validates :name, :email, :password_digest, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  
+  
+end
