c86cabedb8b331a85c538aafb4225b124dd9fc97
LouisOkwaro <okwarolouis@gmail.comgit config --global user.email okwarolouis@gmail.com>
2023-03-05 15:57:44 +0300

    loop to create random number of Skill records for a user

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,11 @@ require 'faker'
     password: 'password'
   )
 
-  
+  # Assign each user some skills (maximum of 10 skills per user)
+  rand(1..10).times do
+    Skill.create!(
+      name: Faker::Job.key_skill,
+      level: rand(1..5),
+      user_id: user.id
+    )
+  end
